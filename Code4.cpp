00fe53da206fe88b86527ae289c04371a4f10128
Shrikar32 <shrikarmujumdar32@gmail.com>
2025-09-30 00:36:24 +0530

    CodeD

---
+++ Code4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Brand {
+protected:
+    string brand;
+public:
+    Brand() {
+        brand = "Tata Motors";
+    }
+    void showBrand() {
+        cout << "Brand: " << brand << endl;
+    }
+};
+
+class Carr : public Brand {
+    string model;
+public:
+    Carr() {
+        cout << "Enter Model: ";
+        cin >> model;
+    }
+    void display() {
+        showBrand();
+        cout << "Model: " << model << endl;
+    }
+};
+
+class Features : public Brand {
+public:
+    void classic() {
+        showBrand();
+        cout << "Features: Safety, Comfort, and Fuel Efficiency" << endl;
+    }
+};
+
+int main() {
+    Carr c;
+    c.display();
+
+    Features f;
+    f.classic();
+
+    return 0;
+}
